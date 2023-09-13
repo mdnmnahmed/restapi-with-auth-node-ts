a1c62243226bdf32263a99496078e599a145fc92
Numan Ahmed <mdnmnahmed@gmail.com>
2023-09-13 09:41:02 +0530

    mongo-db connection wit mongoose

--- src/index.ts
+++ src/index.ts
@@ -4,17 +4,37 @@ import bodyParser from 'body-parser';
 import cookieParser from 'cookie-parser';
 import compression from 'compression';
 import cors from 'cors';
-
+import mongoose from 'mongoose';
+import * as dotenv from 'dotenv';
+import routers from './routers';
+// import { MONGODB_URI } from './config/keys';
 
 const app = express();
 
 // Middlewares
+dotenv.config();
 app.use(cors({
     credentials: true
 }));
 app.use(compression());
 app.use(cookieParser());
 app.use(bodyParser.json());
+
+
+//Connection With DataBase
+// mongoose.connect(process.env.MONGODB_URI || MONGODB_URI,
+//     {
+//         useNewUrlParser: true,
+//         useUnifiedTopology: true,
+//         useFindAndModify: false
+//     })
+//     .then((data)=> console.log("Connected with DB."))
+//     .catch((err) => console.log("Error Occured With DataBase: " + err));
+const MONGODB_URI: string = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.qzgcd.mongodb.net/${process.env.DB_DATABASE}?retryWrites=true&w=majority`;
+mongoose.Promise = Promise;
+mongoose.connect(MONGODB_URI);
+mongoose.connection.on('error', (error: Error) => console.log("Error while connecting with DB: ", error))
+
 
 const server = http.createServer(app);
 server.listen(process.env.PORT || 5000, () => {
