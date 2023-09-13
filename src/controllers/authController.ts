2c6d8ef8daa5e997d99220333c93649fc79cab04
Numan Ahmed <mdnmnahmed@gmail.com>
2023-09-13 09:44:23 +0530

    auth signupUser controller

---
+++ src/controllers/authController.ts
@@ -0,0 +1,33 @@
+import express from 'express';
+import { createUser, getUserByEmail } from '../models/userModel';
+import { authenticationPassword, randomString } from '../helpers/authHelper';
+
+export const signupUser = async (req: express.Request, res: express.Response) => {
+    try {
+        const { email, password, username } = req.body;
+
+        if (!email || !password || !username) {
+            return res.sendStatus(400);
+        }
+
+        const isExistingUser = await getUserByEmail(email);
+        if (isExistingUser) {
+            return res.sendStatus(400);
+        }
+
+        const salt = randomString();
+        const newUser = await createUser({
+            email,
+            username,
+            authentication: {
+                salt,
+                password: authenticationPassword(salt, password)
+            }
+        });
+
+        return res.status(201).json({ message: 'Signup success', data: newUser }).end();
+    } catch (error) {
+        console.log("error: ", error);
+        return res.sendStatus(400);
+    }
+}
